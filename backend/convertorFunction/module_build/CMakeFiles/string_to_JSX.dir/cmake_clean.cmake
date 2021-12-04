bee657b0ef1b8e07d87183026205c9ba32c1ce17
pSN0W <08yaksh08@gmail.com>
2021-12-05 01:32:00 +0530

    Added the python module

---
+++ backend/convertorFunction/module_build/CMakeFiles/string_to_JSX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/string_to_JSX.dir/main.cpp.o"
+  "string_to_JSX.cpython-38-x86_64-linux-gnu.so"
+  "string_to_JSX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/string_to_JSX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
