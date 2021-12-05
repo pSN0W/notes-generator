c0cff393a7da1fcd2601eab933e8435c945a81a0
pSN0W <08yaksh08@gmail.com>
2021-12-05 20:34:15 +0530

    created endpoint for API

---
+++ backend/base/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.get_routes,name="routes"),
+    path('notes/',views.get_notes,name="notes"),
+    path('notes/create',views.create_notes,name="notes-create"),
+    path('notes/<slug:pk>',views.NotesAPIView.as_view(),name="notes-specific"),
+    path('login/', views.MyTokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('users/',views.get_users,name="users"),
+    path('users/create',views.create_user,name="users-create"),
+    path('users/user',views.UserAPIView.as_view(),name="users-specific")
+]
