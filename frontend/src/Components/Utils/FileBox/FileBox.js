cddc8fbb2da87cebd01a5b1e6a229be5cfa6f08f
pSN0W <08yaksh08@gmail.com>
2021-12-07 13:30:42 +0530

    connected the file box to bacckend

--- frontend/src/Components/Utils/FileBox/FileBox.js
+++ frontend/src/Components/Utils/FileBox/FileBox.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { MDBInput } from 'mdbreact';
+
 import FileInfo from './FileInfo/FileInfo';
 import './FileBox.css';
-import { globalFilesData } from '../Data/globalFiles';
-import { userFilesData } from '../Data/userFiles';
+import { listNotes, createNotes } from '../../../Action/notesAction';
 
 /*
 	This component display all the file associated with the user
 */
 function FileBox({ setVisible, activeIndex }) {
+	const dispatch = useDispatch();
+
+	// states for creating notes
+	const {
+		loading: createLoading,
+		success: createSuccess,
+		createdProduct: createdProduct,
+		error: createError
+	} = useSelector((state) => state['notesCreate']);
+
+	// states for getting notes list
+	const { error, loading, notes } = useSelector(
+		(state) => state['notesList']
+	);
 
 	// the index of tab which is currently open
 	const [value, setValue] = useState(activeIndex);
@@ -22,21 +38,38 @@ function FileBox({ setVisible, activeIndex }) {
 	// options for the tab
 	const options = ['Recent', 'Favourites', 'Shared', 'Global'];
 
+	// once note has been created successfully redirect to that page
+	useEffect(() => {
+		if (createSuccess) {
+			window.location.href = `/notes/${createdProduct.id}`;
+		}
+	}, [createSuccess]);
+
+	// get the list of notes from the api when this component is rendered
+	useEffect(() => {
+		dispatch(listNotes());
+	}, []);
+
+	// dispatch creating new notes without any data
+	const handleCreateNew = () => {
+		dispatch(createNotes({}));
+	};
+
 	useEffect(() => {
 		let newData;
 		// setting newData based on the opened tab
 		switch (value) {
 			case 0:
-				newData = userFilesData.own;
+				newData = notes.own;
 				break;
 			case 1:
-				newData = userFilesData.own.filter((file) => file.favourite);
+				newData = notes.own?.filter((file) => file.favourite);
 				break;
 			case 2:
-				newData = globalFilesData;
+				newData = notes.shared;
 				break;
 			case 3:
-				newData = userFilesData.shared;
+				newData = notes.global;
 				break;
 			default:
 				setData(data);
@@ -44,7 +77,7 @@ function FileBox({ setVisible, activeIndex }) {
 		}
 
 		// filtering the newData based on the input in the search box
-		const finData = newData.filter((file) =>
+		const finData = newData?.filter((file) =>
 			file.title.toLowerCase().includes(input.toLowerCase())
 		);
 
@@ -90,12 +123,17 @@ function FileBox({ setVisible, activeIndex }) {
 				/>
 				{/* Map over the notes in data and create a FileInfo component for each of them */}
 				<div className="filebox-file-info">
-					{data.map((file) => (
+					{data?.map((file) => (
 						<FileInfo value={value} {...file} key={file.id} />
 					))}
 				</div>
 				<div className="filebox-footer">
-					<div className="filebox-footer-btn">Create Notes</div>
+					<div
+						className="filebox-footer-btn"
+						onClick={handleCreateNew}
+					>
+						Create Notes
+					</div>
 					<div
 						onClick={() => setVisible(false)}
 						className="filebox-footer-btn"
